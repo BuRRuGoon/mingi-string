e87969e571ec27e782b47cf15222c37fc80b7fd3
BuRRuGoon <alsrl7503@naver.com>
2019-03-22 11:55:27 +0900

    3월22일수정

---
+++ src/main/java/org/mingi/jdbc/spring/Greeter.java
@@ -0,0 +1,10 @@
+package org.mingi.jdbc.spring;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance created!");
+	}
+	public String greet() {
+		return "Hello, World!";
+	}
+}
