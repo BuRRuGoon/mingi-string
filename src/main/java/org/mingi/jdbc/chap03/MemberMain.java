dd70986bc5874ba9b8e631764e0db698b9d9157b
BuRRuGoon <alsrl7503@naver.com>
2019-03-29 11:51:12 +0900

    3/29

--- src/main/java/org/mingi/jdbc/chap03/MemberMain.java
+++ src/main/java/org/mingi/jdbc/chap03/MemberMain.java
@@ -1,5 +1,15 @@
 package org.mingi.jdbc.chap03;
 
 public class MemberMain {
+	public static void main(String[] args) {
+		MemberDao memberDao = new MemberDao();
+		MemberRegisterService regService = new MemberRegisterService(memberDao);
 
+		RegisterRequest req = new RegisterRequest();
+		req.setEmail("alsrl7503@naver.co");
+		req.setPassword("7503");
+		req.setName("BuRRu");
+
+		regService.regist(req);
+}
 }
